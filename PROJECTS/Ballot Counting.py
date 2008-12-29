0fac98ab84a65a504c3a0655b7ae597eec564639
Ibrahim-Eemaan <taofeekibrahimope@gmail.com>
2008-12-29 06:57:47 +0100

    nothing special

--- PROJECTS/Ballot Counting.py
+++ PROJECTS/Ballot Counting.py
@@ -1,1 +1,27 @@
-polls = ['Ben', 'Andy', 'Ben', 'Andy', 'Carol', 'Carol', 'Andy', 'Ben', 'Andy', 'Carol', 'Andy', 'Carol']
+polls = ['Ben', 'Andy', 'Ben', 'Andy', 'Carol', 'Carol', 'Carol', 'Carol', 'Andy', 'Ben', 'Andy', 'Carol', 'Andy', 'Carol']
+#candidate list to store new candidates
+candidates = []
+#votes of candidate of the same index
+votes = []
+for person in polls:
+    if person not in candidates:
+        candidates.append(person)
+        votes.append(1)
+    else:
+        candidate_index = candidates.index(person)
+        votes[candidate_index] += 1
+max_vote = 0
+max_candidates = []
+for i in range(len(votes)):
+    if votes[i] > max_vote:
+        max_vote = votes[i]
+        candidate = candidates[i]
+        #overwrite original list with just 1 candidate
+        max_candidates = []
+        max_candidates.append(candidate)
+    elif votes[i] == max_vote:
+        candidates = candidates[i]
+        max_candidates.append(candidate)
+print('The highest vote goes to: ')
+print(*max_candidates, sep='\n')
+print('Each person has ' + str(max_vote) + ' votes')
