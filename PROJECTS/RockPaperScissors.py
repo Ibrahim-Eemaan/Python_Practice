eb5898f72fc6e61c066f9b587cb604db017afbd0
Ibrahim-Eemaan <taofeekibrahimope@gmail.com>
2008-12-29 05:31:17 +0100

    RockPaperScissors.py

--- PROJECTS/RockPaperScissors.py
+++ PROJECTS/RockPaperScissors.py
@@ -1,36 +1,32 @@
 import random
 
-draws = 0
 wins = 0
+draws = 0
 losses = 0
 is_ended = False
-prompt = "Choose Rock(r), Paper(p), Scissors(s) or 'q' to quit: "
-
+prompt =  "Choose one, Rock ('r'), Paper ('p'), Scissors('s') or 'q' to quit: "
 while True:
     user_choice = input(prompt)
-    while user_choice not in ['r', 'p', 's', 'q']:
+    while user_choice not in (['r', 's', 'p', 'q']):
         user_choice = input(prompt)
     if user_choice == 'q':
         break
     else:
-        computer_choice = random.choice(['r', 'p', 's'])
+        computer_choice = random.choice(['r', 's', 'p', 'q'])
         if computer_choice == 'r':
-            move = 'rock'
-        elif computer_choice == 'p':
-            move = 'paper'
+            move = 'Rock'
+        elif computer_choice == 's':
+            move = 'Scissors'
         else:
-            move = 'scissors'
-        print('computer give a ' + move)
-    if computer_choice == user_choice:
-        print('Draw')
-        draws += 1
-    elif (computer_choice == 'r' and user_choice == 'p') or \
-        (computer_choice == 'p' and user_choice == 's') or \
-             (computer_choice == 's' and user_choice == 'r'):
-             print('You win')
-             wins += 1
-    else:
-        print('You lose!')
-        losses += 1
-
-print('You have ' + str(wins) + ' wins, ' + str(draws) + ' draws, and ' + str(losses) + ' losses.')
+            move = 'Paper'
+        print("Computer take a " + move)
+        if computer_choice == user_choice:
+            print('You Draw')
+            draws += 1
+        if (computer_choice == 'r' and user_choice == 'p' ) or (computer_choice == 'p' and user_choice == 's') or (computer_choice == 's' and user_choice == 'r'):
+            print('YOu win')
+            wins += 1
+        else:
+            print('You lose')
+            losses += 1
+print('You have ' + str(wins) + ' wins, ' + str(draws) + ' draws, ' + str(losses) + ' losses.')
