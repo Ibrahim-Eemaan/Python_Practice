8231022a7d99d9b07f5f1a55df20d2d164a979e2
Ibrahim-Eemaan <taofeekibrahimope@gmail.com>
2020-05-24 02:50:19 +0100

    Initial commit

---
+++ one.py
@@ -0,0 +1,23 @@
+word_count = {
+    'l': 3,
+    'like': 1,
+    'the': 3
+}
+
+#list.item()
+print(word_count.items())
+print(list(word_count.items()))
+
+#list.keys()
+print(word_count.keys())
+
+#list.value()
+print(word_count.values())
+
+#list.remove()
+word_count.pop('l')
+print(word_count)
+
+#pop item
+word_count.popitem
+print(word_count)
