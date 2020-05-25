9d973cd9ccab92a127c7c4445cd2fd62e995c0cc
Ibrahim-Eemaan <taofeekibrahimope@gmail.com>
2020-05-25 01:26:17 +0100

    Added Change

--- one.py
+++ one.py
@@ -21,3 +21,31 @@ print(word_count)
 #pop item
 word_count.popitem
 print(word_count)
+
+
+#MUTABILITY
+
+#lists[] are mutable
+#tuples() are immutable
+#t = (1,2,3) this is unchangeable, just like rock
+#l = [1,2,3] this is changeable
+
+#IMMUTABLES
+#tuples, floats, booleans
+
+#MUTABLE
+#list, dictionaries, ordereddict
+
+
+
+#REGEX - REGEX - REGEX
+
+import re
+
+text = 'random string. myname.@gmail.com'
+
+pattern = re.compile('[@]')
+
+result = pattern.search(text)
+
+print(result)
